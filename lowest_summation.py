712d03bd735854510d679d59da7a4ec8f38cdb42
Justin Hammer <justhamr@gmail.com>
2017-07-04 18:01:00 -0600

    changed to else instead of elif.

--- lowest_summation.py
+++ lowest_summation.py
@@ -5,7 +5,7 @@
 # to create a new unique value and add that to the
 # running total.
 # Eg: One of the 2s becomes a three and the list
-# becomes [1,2,3,4,5] with a total sum of 15.
+# becomes [1,2,2,2,3,4,5] with a total sum of 15.
 
 
 def lowest_summation_of_unique_values(arr):
@@ -16,7 +16,7 @@ def lowest_summation_of_unique_values(arr):
         if n not in seen:
             seen.add(n)
             total += n
-        elif n in seen:
+        else:
             while n in seen:
                 n = n+1
             seen.add(n) # This line can be removed if we don't care about n+1 being added to our list of unique values.
